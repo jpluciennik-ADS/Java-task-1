3848161f604dd61d57efccebf09d01a77184f7e7
jpluciennik-ADS <jpluciennik@deloittece.com>
2022-07-25 15:55:26 +0200

    Mock data created

---
+++ task-1/src/main/java/org/mockData/mockStudents.java
@@ -0,0 +1,23 @@
+package org.mockData;
+
+import org.students.FullTimeStudent;
+import org.students.PartTimeStudent;
+import org.students.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class mockStudents {
+    public static List<Student> mockStudents() {
+        Student fullTime1 = new FullTimeStudent("27014", "Andrzej", "Krasiecki");
+        Student fullTime2 = new FullTimeStudent("20901", "Martyna", "Podgorska");
+        Student fullTime3 = new FullTimeStudent("25821", "Matylda", "Bak");
+        Student parTime1 = new PartTimeStudent("26665", "Aleksandra", "Kruk");
+        Student parTime2 = new PartTimeStudent("28804", "Krzysztof", "Alabama");
+        Student parTime3 = new PartTimeStudent("29999", "Pawel", "Piotr");
+
+        List<Student> listOfStudents = new ArrayList<>(List.of(fullTime1, fullTime2, fullTime3, parTime1, parTime2, parTime3));
+        return listOfStudents;
+    }
+
+}
